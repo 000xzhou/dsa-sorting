b3cbc66cdf54090131f231b38d6508eda7d21b25
000xzhou <xzhou92@gmail.com>
2024-06-09 13:09:49 -0400

    merage sort

--- merge.js
+++ merge.js
@@ -1,5 +1,34 @@
-function merge() {}
+function merge(arr1, arr2) {
+  let arr = new Array();
+  let i = 0;
+  let j = 0;
 
-function mergeSort() {}
+  while (arr1[i] != undefined && arr2[j] != undefined) {
+    if (arr1[i] <= arr2[j]) {
+      arr.push(arr1[i]);
+      i++;
+    } else {
+      arr.push(arr2[j]);
+      j++;
+    }
+  }
 
-module.exports = { merge, mergeSort};
+  arr.push(...arr1.slice(i));
+  arr.push(...arr2.slice(j));
+
+  return arr;
+}
+
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  let mid = Math.floor(arr.length / 2);
+
+  let x = mergeSort(arr.slice(0, mid));
+  let y = mergeSort(arr.slice(mid));
+
+  return merge(x, y);
+}
+
+module.exports = { merge, mergeSort };
