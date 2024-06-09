2d456a920392dc05ac8fd10472e6635901e518f5
000xzhou <xzhou92@gmail.com>
2024-06-09 10:11:17 -0400

    selection

--- selection.js
+++ selection.js
@@ -1,3 +1,19 @@
-function selectionSort() {}
+function selectionSort(arr) {
+  console.log(arr);
+  for (let j = 0; j < arr.length - 1; j++) {
+    let min = j;
+    for (let i = min + 1; i < arr.length; i++) {
+      // if j+1 is less than j
+      if (arr[i] < arr[min]) {
+        min = i;
+      }
+    }
+    // swap
+    [arr[j], arr[min]] = [arr[min], arr[j]];
+
+    // console.log("j:", j, arr[j], "min", min, arr[min]);
+  }
+  return arr;
+}
 
 module.exports = selectionSort;
